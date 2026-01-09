00d2b95d54b5a73e80f205cc1fb0726df7a27036
Danilo Silva <danilodn.212@gmai.com>
2026-01-09 13:33:36 -0300

    Fnalizado exercicio Ordenação de Pessoas

---
+++ src/List/Ordenação/OrdenaçãoPessoa.java
@@ -0,0 +1,30 @@
+    package List.Ordenação;
+
+    import java.util.ArrayList;
+    import java.util.Collections;
+    import java.util.List;
+
+    public class OrdenaçãoPessoa {
+        private List<Pessoa> listaPessoas;
+
+        public OrdenaçãoPessoa(){
+            listaPessoas = new ArrayList<>();
+        }
+
+        public void adicionarPessoa(String nome, int idade, double altura){
+            Pessoa pessoa = new Pessoa(nome, idade, altura);
+            listaPessoas.add(pessoa);
+        }
+
+        public List<Pessoa> ordenarPorIdade(){
+            List<Pessoa> pessoasPorIdade = new ArrayList<>(listaPessoas);
+            Collections.sort(pessoasPorIdade);
+            return pessoasPorIdade;
+        }
+
+        public List<Pessoa> ordenarPorAltura(){
+            List<Pessoa> pessoasPorAltura = new ArrayList<>(listaPessoas);
+            Collections.sort(pessoasPorAltura, new ComparatorAltura());
+            return pessoasPorAltura;
+        }
+    }
